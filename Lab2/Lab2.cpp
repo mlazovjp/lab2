b0ded4cb979373cb908209d34e404cec66178442
mlazo <mlazovjp@gmail.com>
2017-02-02 23:47:19 -0500

    Now outputs to console details of IMAGE_SECTION_HEADER

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -13,37 +13,28 @@ int i = 0;
 // First step towards creating the .bss section - End
 
 
-bool openMyself(FILE *fp) {
+// "borrowed" from http://www.rohitab.com/discuss/topic/37906-how-to-get-address-of-a-pe-data-section/
+IMAGE_SECTION_HEADER *find_section(IMAGE_NT_HEADERS *nthdrs, const char *sec_name)
+{
+	IMAGE_SECTION_HEADER *sec_hdr = IMAGE_FIRST_SECTION(nthdrs);
+	WORD nsections = nthdrs->FileHeader.NumberOfSections;
 
-	TCHAR szPath[MAX_PATH];
-
-	if (!GetModuleFileName(NULL, szPath, MAX_PATH))
-	{
-		printf("Cannot determine the path of this executable\n");
-		return false;
+	while (nsections--) {
+		if (strcmp((char *)sec_hdr->Name, sec_name) == 0)
+			return sec_hdr;
+		sec_hdr++;
 	}
 
-	printf("\nszPath=%ls\n", szPath);
-	
-	TCHAR *mode = TEXT("r");			// open this file in read-only mode.  r+ produces "Permission denied" error
-	char ch;
-	_wfopen_s(&fp, szPath, mode);
-	
-	if (fp == NULL)
-	{
-		perror("Error While Opening The File Or File Does Not Exist\n");
-		exit(EXIT_FAILURE);
-	}
-
-	while ((ch = fgetc(fp)) != EOF)
-		printf("%c", ch);
-
-	return true;
+	return NULL;
 }
 
-bool closeMyself(FILE *fp) {
-	fclose(fp);
-	return true;
+
+void outputDetailsOfIMAGE_SECTION_HEADER(IMAGE_SECTION_HEADER &ish) {
+
+	printf("\n%s\n", ish.Name);
+	printf("\tSizeOfRawData (bytes)\t= %#010x (%d)\n", ish.SizeOfRawData, ish.SizeOfRawData);
+	printf("\tPointerToRawData\t= %#010x\n", ish.PointerToRawData);
+
 }
 
 
@@ -53,12 +44,27 @@ int main(int argc, char *argv[]) {
 
 	i = 3;					// second step required for creating .bss section
 
-	FILE fp;				
-	openMyself(&fp);		// open this binary in read-only mode
+	// "borrowed" from http://stackoverflow.com/questions/6126980/get-pointer-to-image-dos-header-with-getmodulehandle
+	PIMAGE_DOS_HEADER pidh = (PIMAGE_DOS_HEADER)GetModuleHandle(NULL);
+
+	// "borrowed" from http://stackoverflow.com/questions/8782771/loading-pe-headers
+	PIMAGE_NT_HEADERS pinh = (PIMAGE_NT_HEADERS)((BYTE*)pidh + pidh->e_lfanew);
+	PIMAGE_FILE_HEADER pifh = (PIMAGE_FILE_HEADER)&pinh->FileHeader;
+	PIMAGE_OPTIONAL_HEADER pioh = (PIMAGE_OPTIONAL_HEADER)&pinh->OptionalHeader;
 	
+	// create image section headers for the sections we care about
+	IMAGE_SECTION_HEADER pish_text = *find_section(pinh, ".text");
+	IMAGE_SECTION_HEADER pish_rdata = *find_section(pinh, ".rdata");
+	IMAGE_SECTION_HEADER pish_data = *find_section(pinh, ".data");
+	IMAGE_SECTION_HEADER pish_bss = *find_section(pinh, "bss");
+
+	// display console output showing details of the sections
+	outputDetailsOfIMAGE_SECTION_HEADER(pish_text);
+	outputDetailsOfIMAGE_SECTION_HEADER(pish_rdata);
+	outputDetailsOfIMAGE_SECTION_HEADER(pish_data);
+	outputDetailsOfIMAGE_SECTION_HEADER(pish_bss);
 
 
-	closeMyself(&fp);
 
 	return 0;
 }
