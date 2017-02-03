9da8df9ce47e305e56a2a844f3668e51056c515f
mlazo <mlazovjp@gmail.com>
2017-02-03 07:28:47 -0500

    Prior to changing how I read contents of memory defined at data-size

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -32,11 +32,35 @@ IMAGE_SECTION_HEADER *find_section(IMAGE_NT_HEADERS *nthdrs, const char *sec_nam
 void outputDetailsOfIMAGE_SECTION_HEADER(IMAGE_SECTION_HEADER &ish) {
 
 	printf("\n%s\n", ish.Name);
-	printf("\tSizeOfRawData (bytes)\t= %#010x (%d)\n", ish.SizeOfRawData, ish.SizeOfRawData);
+	printf("\tSizeOfRawData in bytes\t= %#010x (%d)\n", ish.SizeOfRawData, ish.SizeOfRawData);
 	printf("\tPointerToRawData\t= %#010x\n", ish.PointerToRawData);
 
 }
 
+//http://stackoverflow.com/questions/15312340/printing-memory-as-1-byte-hex-in-c
+/*
+void printMemoryAtAddress(void)
+{
+	int dumpSize = 15;
+
+	int input;
+	printf("Enter the address (without the 0x):");
+	scanf("%x", &input);
+	printf("You entered: %x \n", input);
+	printf("Address \tByte value \n");
+
+	int i;
+	for (i = 0; i<dumpSize; i++)
+	{
+		//int* address;
+		unsigned char *address;
+		address = (int*)(input + i);
+		address = (int*)(input + i);
+		printf("%p \t", address);
+		printf("%.2x \n", *address);
+	}
+}
+*/
 
 int main(int argc, char *argv[]) {
 
@@ -64,7 +88,57 @@ int main(int argc, char *argv[]) {
 	outputDetailsOfIMAGE_SECTION_HEADER(pish_data);
 	outputDetailsOfIMAGE_SECTION_HEADER(pish_bss);
 
+	// borrowed from https://bytes.com/topic/c/answers/821797-jump-address-syntax
+	//unsigned long address = 0x400000;
+	//unsigned long address = pish_text.PointerToRawData;
+	//typedef void VFunc(void);
+	//((VFunc *)address)();
 
+	/*
+	unsigned char *p = (unsigned char *)&address;
+	int i;
+
+	printf("\nContents of %#010x \t=", address);
+	
+	for (i = 0; i < sizeof(address); ++i)
+	{
+		printf(" %02x", p[i]);
+	}
+	
+	printf("\n");
+	*/
+
+	/*
+	int n = 8, i = 0;
+	unsigned char* byte_array = (unsigned char*)&pish_text.PointerToRawData;
+	
+	printf("\nContents of %#010x \t\t= ", address);
+	while (i < n)
+	{
+		printf("%02X ", (unsigned)byte_array[i]);
+		i++;
+	}
+	printf("\n");
+	*/
+	/*
+	DWORD  dw = 0x00000400;
+	unsigned char* pc = (unsigned char*)&dw;
+	for (int i = 3; i >= 0; i--) {
+		printf("%02X ", (unsigned)pc[i]);
+	}
+	*/
+
+
+	// pointer to int (4bytes) that points to memory address 0x611268
+	int* address = (int *)0x000400;
+
+	printf("Memory address is: 0x%x\n", address);
+
+	// Note that this address should exist on your process' memory or 
+	// the line below will cause a Segmentation Fault
+	*address = 0xdead; //assign a value to that address
+
+	printf("Content of that address is: 0x%x\n", *address);
 
 	return 0;
 }
