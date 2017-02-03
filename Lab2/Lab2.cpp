973e45897acedc24da337072fa4d82429948e840
mlazo <mlazovjp@gmail.com>
2017-02-02 20:19:38 -0500

    Closes #1 Code now produces binary .exe file which can open itself in read-only mode

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -2,9 +2,9 @@
 //
 
 #include "stdafx.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include "Windows.h"
-//#include "WinNT.h"
 
 // First step towards creating the .bss section - Start
 #pragma section("bss",read,write)  
@@ -14,26 +14,40 @@ int i = 0;
 
 int main(int argc, char *argv[]) {
 
-	int intArray[30];
+	//int intArray[30];
 
 	i = 3;			// second step required for creating .bss section
 
-	/*const char *path;
-	char *drive;
-	char *dir;
-	char *fname;
-	char *ext;
-	_splitpath(path, drive, dir, fname, ext);
-	*/
 
-	char path_buffer[_MAX_PATH];
-	char drive[_MAX_DRIVE];
-	char dir[_MAX_DIR];
-	char fname[_MAX_FNAME];
-	char ext[_MAX_EXT];
-	errno_t err;
+	TCHAR szPath[MAX_PATH];
 
-	printf("argv[0]=%s", argv[0]);
+	if (!GetModuleFileName(NULL, szPath, MAX_PATH))
+	{
+		printf("Cannot determine the path of this executable\n");
+		return -1;
+	}
+
+	printf("\nszPath=%ls\n", szPath);
+
+	//wcscpy_s(szPath, _countof(szPath), TEXT("C:\\Users\\mlazo\\Dropbox\\MCS\\CPS592 (Rev Eng)\\Assignments\\02\\source\\Lab2\\Release\\Lab2.exea"));
+	//wcscpy_s(szPath, _countof(szPath), TEXT("C:\\Users\\mlazo\\Dropbox\\MCS\\CPS592 (Rev Eng)\\Assignments\\02\\source\\Lab2\\Release\\Lab2.exe"));
+	//printf("szPath after=%ls\n", szPath);
+
+	TCHAR *mode = TEXT("r");			// open this file in read-only mode.  r+ produces "Permission denied" error
+	char ch;
+	FILE *fp;
+	_wfopen_s(&fp, szPath, mode);
+
+	if (fp == NULL)
+	{
+		perror("Error While Opening The File Or File Does Not Exist\n");
+		exit(EXIT_FAILURE);
+	}
+
+	while ((ch = fgetc(fp)) != EOF)
+		printf("%c", ch);
+
+	fclose(fp);
 
 	return 0;
 }
